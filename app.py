0b10879e95784c16ae291d9bbbb08118433efb5b
Karthikpanga <92092813+Karthikpanga@users.noreply.github.com>
2022-12-22 11:45:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-import streamlit as st
-from pytableau import TableauAuth, Server, Site, Workbook
-import pandas as pd
-# Set Tableau server URL, username, and password
-server_url = "https://prod-useast-a.online.tableau.com"
-username = "user@example.com"
-password = "password"
-# Authenticate with Tableau server
-auth = TableauAuth(username, password)
-# Connect to Tableau server
-server = Server(server_url)
-# Get information about the Tableau site
-site = Site(server, "Site21")
-# Get the desired workbook
-workbook = Workbook(server, site, "Workbook Name")
-# Get the data from the workbook as a Pandas DataFrame
-df = workbook.fetch_data()
-# Display the data in a line chart
-st.line_chart(df)
+import tableauserverclient as TSC
+
+server_url = 'https://prod-useast-a.online.tableau.com'
+user = 'amulya.s.nidhi@kipi.bi'
+password = 'Kipithon@123'
+site = 'Site21'
+
+tableau_auth = TSC.TableauAuth(username=user, password=password, site_id=site)
+server = TSC.Server(server_url, use_server_version=True)
+
+with server.auth.sign_in(tableau_auth):
+    print('[Logged in successfully to {}]'.format(server_url))
