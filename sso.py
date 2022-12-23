6518ce222e882c1b8fc5d7e1d250cfcf389d56cb
Karthikpanga <92092813+Karthikpanga@users.noreply.github.com>
2022-12-23 10:43:19 +0530

    Update sso.py

--- sso.py
+++ sso.py
@@ -36,5 +36,35 @@ def check_password():
         return True
 
 if check_password():
-    st.write("Here goes your normal Streamlit app...")
-    st.button("Click me")
+    st.markdown ("#### Dashboard Il")
+    server_url = 'https://prod-useast-a.online.tableau.com'
+    user = 'amulya.s.nidhi@kipi.bi'
+    password = 'Kipithon@123'
+    site = 'Site21'
+
+    tableau_auth = TSC.TableauAuth(username=user, password=password, site_id=site)
+    server = TSC.Server(server_url, use_server_version=True)
+
+    # Fetch the dashboards/charts
+    @st.experimental_memo(ttl=1200)
+    def run_query(view_name):
+        with server.auth.sign_in(tableau_auth):
+            workbooks, pagination_item = server.workbooks.get()
+            for w in workbooks:
+                if w.name == 'ourworkbook':
+                    our_workbook = w
+                    break
+    # Get views for trydashboard workbook.
+            server.workbooks.populate_views(our_workbook)
+            for v in our_workbook.views:
+                if view_name == v.name:
+                    our_view = v
+                    break
+            #Get an image for the view.
+            server.views.populate_image(our_view)
+            view_image = our_view.image
+            return view_image
+    
+    view_image = run_query('MyDash')
+    st.image(view_image, width=800)
+    
