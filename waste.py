2474b20118d84e8891cfcef43d652753af19ddb5
Karthikpanga <92092813+Karthikpanga@users.noreply.github.com>
2023-01-04 13:16:55 +0530

    Update waste.py

--- waste.py
+++ waste.py
@@ -49,43 +49,43 @@ from streamlit_option_menu import option_menu
                             # Password correct.
                             return True
 
-if check_password():
-  with st.expander('Tableau'):
-    
-    if st.session_state["username"] == 'streamlit1':
-       st.markdown ("#### Dashboard")
-       server_url = 'https://prod-useast-a.online.tableau.com'
-       user = 'amulya.s.nidhi@kipi.bi'
-       password = 'Kipithon@123'
-       site = 'Site21'
+    if check_password():
+      with st.expander('Tableau'):
 
-       tableau_auth = TSC.TableauAuth(username=user, password=password, site_id=site)
-       server = TSC.Server(server_url, use_server_version=True)
+        if st.session_state["username"] == 'streamlit1':
+           st.markdown ("#### Dashboard")
+           server_url = 'https://prod-useast-a.online.tableau.com'
+           user = 'amulya.s.nidhi@kipi.bi'
+           password = 'Kipithon@123'
+           site = 'Site21'
 
-       # Fetch the dashboards/charts
-       @st.experimental_memo(ttl=1200)
-       def run_query(view_name):
-         with server.auth.sign_in(tableau_auth):
-           workbooks, pagination_item = server.workbooks.get()
-           for w in workbooks:
-             if w.name == 'ourworkbook':
-               our_workbook = w
-               break
-       # Get views for trydashboard workbook.
-           server.workbooks.populate_views(our_workbook)
-           for v in our_workbook.views:
-             if view_name == v.name:
-               our_view = v
-               break
-       #Get an image for the view.
-           server.views.populate_image(our_view)
-           view_image = our_view.image
-           return view_image
+           tableau_auth = TSC.TableauAuth(username=user, password=password, site_id=site)
+           server = TSC.Server(server_url, use_server_version=True)
 
-       view_image = run_query('MyDash')
-       st.image(view_image, width=800)
-    else:
-        st.write("bye!")
+           # Fetch the dashboards/charts
+           @st.experimental_memo(ttl=1200)
+           def run_query(view_name):
+             with server.auth.sign_in(tableau_auth):
+               workbooks, pagination_item = server.workbooks.get()
+               for w in workbooks:
+                 if w.name == 'ourworkbook':
+                   our_workbook = w
+                   break
+           # Get views for trydashboard workbook.
+               server.workbooks.populate_views(our_workbook)
+               for v in our_workbook.views:
+                 if view_name == v.name:
+                   our_view = v
+                   break
+           #Get an image for the view.
+               server.views.populate_image(our_view)
+               view_image = our_view.image
+               return view_image
+
+           view_image = run_query('MyDash')
+           st.image(view_image, width=800)
+        else:
+            st.write("bye!")
                     
         
             elif choose == 'ml':
